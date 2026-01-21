301f8b210774442e45645c22bf6bbdc64660835d
Ke-Chieh Chang <kechiec@uci.edu>
2026-01-20 18:06:52 -0800

    Activity 1 & 2 completed

--- GDIM32 In-Class Activities/Assets/W3/Scripts/InheritanceTest.cs
+++ GDIM32 In-Class Activities/Assets/W3/Scripts/InheritanceTest.cs
@@ -1,4 +1,3 @@
-/*
 using UnityEngine;
 
 public class InheritanceTest : MonoBehaviour
@@ -32,14 +31,18 @@ public class Npc
 }
 
 // CHANGE THINGS IN VILLAGER...
-public class Villager
+public class Villager : Npc
 {
-	
+	public override void Talk(string playerName) {
+		Debug.Log(playerName + ", Hi");
+	}
 }
 
 // CHANGE THINGS IN ENEMY...
-public class Enemy
+public class Enemy : Npc
 {
-	
+    public override void Talk(string playerName)
+    {
+        Debug.Log(playerName + ", nice to meet you :<");
+    }
 }
-*/
--- GDIM32 In-Class Activities/Assets/W3/Scripts/Pigeon.cs
+++ GDIM32 In-Class Activities/Assets/W3/Scripts/Pigeon.cs
@@ -9,6 +9,7 @@ public class Pigeon : MonoBehaviour
     [SerializeField] private Animator _animator;
 
     // (1) add a member variable to represent the Pigeon's state
+    private MovementState _movementState;
     
 
     void Update()
@@ -22,7 +23,12 @@ public class Pigeon : MonoBehaviour
     // - otherwise, it should be Idle
     private void UpdateState ()
     {
-        
+        if (Input.GetKey(KeyCode.A)) {
+            _movementState = MovementState.Flying;
+        }
+        else{
+            _movementState = MovementState.Idle;
+        }
     }
 
     // (3) fill in this method to update the pigeon's animation based on its state
@@ -31,7 +37,10 @@ public class Pigeon : MonoBehaviour
     // use a Switch statement!
     private void UpdateAppearance()
     {
-        
+        if(_movementState == MovementState.Idle)
+            PlayIdleAnimation();
+        if(_movementState == MovementState.Flying)
+            PlayFlyAnimation();
     }
 
     private void PlayFlyAnimation () {
